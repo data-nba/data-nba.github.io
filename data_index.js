b9c33395e049251f4654a764abc7f9688c5c250e
Carlitonchin <carlozalejandro98@gmail.com>
2021-10-17 18:39:56 +0200

    Ajax sincrono

--- data_index.js
+++ data_index.js
@@ -1,35 +1,5 @@
-function good(callback, data)
-{
-    console.log("data")
-    document.querySelector('.loading').style.display = 'none'
-    var data = JSON.parse(xhr.responseText);
-    callback(data)
-}
+var xhr = new XMLHttpRequest();
+xhr.open("GET","fichero.txt", false);
+xhr.send(null);
+var data = JSON.parse(xhr.responseText)
 
-function notGood()
-{
-console.log("not good")
-}
-
-function data(callback)
-{
-    console.log("ok")
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET","data.txt", true);
-    xhr.onreadystatechange = function()
-    {  if (xhr.readyState === 4) {
-        var status = xhr.status;
-        if ((status >= 200 && status < 300) || (status === 304))
-        {
-            window.onload = good.bind(this, callback, data)
-        }
-        else
-        {
-            window.onload = notGood
-        }
-
-    }
- };
-
- xhr.send(null)
-}
