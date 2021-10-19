ad247184cfd038b9750f38d5d9d82cb3e428e44c
Carlos Alejandro <carlozalejandro98@gmail.com>
2021-10-19 17:27:02 -0400

    fetch error

--- data_index.js
+++ data_index.js
@@ -1,23 +1,4 @@
-var xhr = new XMLHttpRequest();
-xhr.addEventListener('loadstart', onLoadStart, false);
-xhr.addEventListener('progress', onProgress, false);
-xhr.addEventListener('load', onLoad, false);
-xhr.addEventListener('error', onError, false);
-xhr.addEventListener('abort', onAbort, false);
-xhr.open('GET', '/data.txt');
-function onLoadStart(evt) {
-console.log('Iniciando la petición');
-}
-function onProgress(evt) {
-var porcentajeActual = (evt.loaded / evt.total) * 100;
-console.log(porcentajeActual);
-}
-function onLoad(evt) {
-console.log(evt);
-}
-function onError(evt) {
-console.error('Error durante la transferenciao');
-}
-function onAbort(evt) {
-console.error('El usuario ha cancelado la petición');
+function fetchData()
+{
+    fetch('/data.txt').then(res=>res.json()).then((result)=>{console.log("hey")}, (error)=>{console.log(error)})
 }
